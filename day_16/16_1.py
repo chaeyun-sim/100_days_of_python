2268e4858de1bd99a18a434da1d95dd048967e0d
Chaeyun Sim <bysimune@gmail.com>
2022-08-28 14:15:33 +0900

    make a coffee machine agagin with modules

---
+++ day_16/16_1.py
@@ -0,0 +1,37 @@
+# learn how to use module
+
+# import another_module
+# print(another_module.another_variable) # 12
+
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle") # changes the shape of an arrow to a turtle
+# timmy.color("coral") # change the color
+# # timmy.forward(100)
+# timmy.circle(0.2)
+
+
+# my_screen = Screen()
+# print(my_screen.canvheight) # the screen pops up about 1 sec
+# my_screen.exitonclick()
+# # when the screen pops up, it disappears when we click the screen # timmiy shows up as an arrow
+
+# print("| Pokemon Name | Type |")
+# print("_______________________")
+
+
+# install :
+# terminal :  python -m pip install -U prettytable 추가
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+
+# # table.align = "l"
+# table.align = "c"
+# # table.align = "r"
+
+# print(table)
+
