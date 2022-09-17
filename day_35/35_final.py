9700bfa685a098985b96143057ad0657b6913698
chaeyunsim <bysimune@gmail.com>
2022-09-18 01:59:11 +0900

    rain alert project

--- day_35/35_final.py
+++ day_35/35_final.py
@@ -1,21 +1,51 @@
 import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
 
-endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-api_key = "2834387742b25d5393a21e88fee8246a"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = os.environ.get("MY_API_KEY")
+account_sid = os.environ.get("MY_SID")
+auth_token = os.environ.get("MY_AUTH_TOKEN")
 
 
 weather_params = {
     "lat" : 35.165608,
-    # "lat" : 37.566536
     "lon" : 128.054650,
-    # "lon" : 126.977966
-    "exclude" : "current,minutely,daily",
+    # "exclude" : "current,minutely,daily",
     "appid" : api_key,
     }
 
 
-response = requests.get(endpoint, params=weather_params)
-print(response.status_code)
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
 weather_data = response.json()
-print(weather_data)
+weather_slice = weather_data["hourly"][:12]
+# print(weather_slice)
+
+will_rain = False
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    try:
+        proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    except KeyError:
+        pass
+    else:
+
+        client = Client(account_sid, auth_token, http_client=proxy_client)
+        
+        message = client.messages \
+            .create(
+                body="내일 비가 온다고 합니다. 우산 챙겨 나가도록 하세요 :)",
+                from_='+19014459477',
+                to='+821031073421'
+                )
+        print(message.status)
+        print("Done!")
+
