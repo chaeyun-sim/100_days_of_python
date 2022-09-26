1d1ad1fcab8f2b5a729053e8c2324bb52d3f4200
chaeyunsim <bysimune@gmail.com>
2022-09-26 17:32:45 +0900

    web scrapping (beautifulsoup)

---
+++ day_45/45_1.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+yc_webpage = response.text
+
+soup = BeautifulSoup(yc_webpage, "html.parser")
+# print(soup)
+articles = soup.find_all(name="a", class_="titlelink")
+# print(story_links)
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvote = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+# print(article_texts)
+# print(article_links)
+# print(article_upvote)
+
+largest_num = max(article_upvote)
+index = article_upvote.index(largest_num)
+famous_text = article_texts[index]
+famous_list = article_links[index]
+print(famous_text, famous_list, largest_num)
