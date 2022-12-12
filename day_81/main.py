21b52584386881d5e7ff83f5ea6f98e936dd1503
chaeyun-sim <bysimune@gmail.com>
2022-12-12 15:52:04 +0900

    Portfolio - Text to Morse Code Converter

---
+++ day_81/main.py
@@ -0,0 +1,52 @@
+# Text to Morse Code Converter
+from art import logo
+
+print(logo)
+
+dic = { 'A' : '.-',
+	'B' : '-...',
+	'C' : '-.-.',
+	'D'  : '-..',
+	'E'    : '.',
+	'F' : '..-.',
+	'G'  : '--.',
+	'H' : '....',
+	'I'   : '..',
+	'J' : '.---',
+    'K'  : '-.-',
+	'L' : '.-..',
+	'M'   : '--',
+	'N'   : '-.',
+	'O'  : '---',
+	'P' : '.--.',
+	'Q' : '--.-',
+	'R'  : '.-.',
+	'S'  : '...',
+	'T'    : '-',
+	'U'  : '..-',
+	'V' : '...-',
+	'W'  : '.--',
+	'X' : '-..-',
+	'Y' : '-.--',
+	'Z' : '--..' }
+
+is_continue = True
+
+def MorseConverter(word):
+    morse_convert = ''
+    for letter in word:
+        try:
+            morse_convert += dic[letter] + ' '
+        except:
+            morse_convert += ''
+    return print(f"Your result is : {morse_convert}")
+
+while is_continue:
+    word = input('Type your word : ').upper()
+    MorseConverter(word)
+
+    again_check = input('\nDo you want to do it again?\nType "y" if yes, other keys if no. ')
+    print('\n')
+    if again_check != 'y':
+        print("Good Bye!")
+        is_continue = False
